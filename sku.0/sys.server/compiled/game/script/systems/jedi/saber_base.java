735d486b9ecec647a4965dd0f2565fec452eeb71
daquorm89 <douweheuvel@gmail.com>
2026-08-14 12:47:13 +0000

    Remove nomove from lightsaber templates; saber_base transfer diagnostic v8
    - crafted_lightsaber_base (1h/2h/polearm): drop item.special.nomove from
      scripts list so new sabers never get the no-trade transfer OVERRIDE.
    - saber_base: OnAboutToBeTransferred always ALLOW onto player + TestingOnly
      so we can see if item scripts run at all on equip.

--- sku.0/sys.server/compiled/game/script/systems/jedi/saber_base.java
+++ sku.0/sys.server/compiled/game/script/systems/jedi/saber_base.java
@@ -49,7 +49,23 @@ public class saber_base extends script.base_script
         }
         return SCRIPT_CONTINUE;
     }
-    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
+        public int OnAboutToBeTransferred(obj_id self, obj_id destContainer, obj_id transferer) throws InterruptedException
+    {
+        if (isPlayer(destContainer))
+        {
+            if (hasScript(self, "item.special.nomove"))
+            {
+                detachScript(self, "item.special.nomove");
+            }
+            if (hasScript(self, "item.special.nomove_base"))
+            {
+                detachScript(self, "item.special.nomove_base");
+            }
+            sendSystemMessageTestingOnly(destContainer, "[precu] saber_base v8 transfer ALLOW tmpl=" + getTemplateName(self));
+        }
+        return SCRIPT_CONTINUE;
+    }
+public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
     {
         if (canManipulate(player, self, false, true, 15, true))
         {
